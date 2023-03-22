c28267c05a194434aef7b50f947bc180cc236b86
burak48 <buraksaglam3@gmail.com>
2023-03-22 22:48:30 +0300

    books are listed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [books, setBooks] = useState([]);
+
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  const handleSearch = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.get(`http://localhost:3001/books?query=${query}`);
+      setBooks(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Search for Books</h1>
+      <form onSubmit={handleSearch}>
+        <input type="text" placeholder="Enter search query" value={query} onChange={handleInputChange} />
+        <button type="submit">Search</button>
+      </form>
+      <div className="book-list">
+        {books.map((book, index) => (
+          book.title.toLowerCase().includes(query.toLowerCase()) ||
+            book.author.toLowerCase().includes(query.toLowerCase()) ||
+            book.publicationYear.toString().includes(query) ? (
+            <div key={index} className="book">
+              <h2>{book.title}</h2>
+              <p>Author: {book.author}</p>
+              <p>Publication Year: {book.publicationYear}</p>
+              <p>Description: {book.description}</p>
+            </div>
+          ) : null
+        ))}
+      </div>
     </div>
   );
 }
